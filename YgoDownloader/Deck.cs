2b63d5c583dfa82430ed5a8b670669ec9db84239
Ivan Kovachev <I.KOVACHEV@rimsoft.bg>
2024-01-25 13:22:05 +0200

    Add project files.

---
+++ YgoDownloader/Deck.cs
@@ -0,0 +1,70 @@
+﻿namespace YgoDownloader;
+
+public class Deck
+{
+    public int Id { get; set; }
+    public object Name { get; set; }
+    public string Ydk { get; set; }
+    public string Builder { get; set; }
+    public string PlayerId { get; set; }
+    public string Type { get; set; }
+    public string Category { get; set; }
+    public string FormatName { get; set; }
+    public int FormatId { get; set; }
+    public string Community { get; set; }
+    public string EventName { get; set; }
+    public int EventId { get; set; }
+    public DateTime PublishDate { get; set; }
+    public int Placement { get; set; }
+    public int Downloads { get; set; }
+    public int Views { get; set; }
+    public int Rating { get; set; }
+    public Format Format { get; set; }
+    public Player Player { get; set; }
+    public DateTime UpdatedAt { get; set; }
+    public Main[] Main { get; set; }
+    public Extra[] Extra { get; set; }
+    public Side[] Side { get; set; }
+}
+
+public class Format
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Icon { get; set; }
+    public string Banlist { get; set; }
+}
+
+public class Player
+{
+    public string Id { get; set; }
+    public string Name { get; set; }
+    public string Discriminator { get; set; }
+    public string DiscordName { get; set; }
+    public string DiscordId { get; set; }
+    public string DiscordPfp { get; set; }
+}
+
+public class Main
+{
+    public string Name { get; set; }
+    public int Id { get; set; }
+    public string YpdId { get; set; }
+    public int SortPriority { get; set; }
+}
+
+public class Extra
+{
+    public string Name { get; set; }
+    public int Id { get; set; }
+    public string YpdId { get; set; }
+    public int SortPriority { get; set; }
+}
+
+public class Side
+{
+    public string Name { get; set; }
+    public int Id { get; set; }
+    public string YpdId { get; set; }
+    public int SortPriority { get; set; }
+}
---
+++ YgoDownloader/Program.cs
@@ -0,0 +1,78 @@
+﻿using System.Diagnostics;
+using System.Net;
+using System.Reflection;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace YgoDownloader
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var deckNumber = Console.ReadLine();
+
+            while (deckNumber != "E")
+            {
+                var deck = await CreateDeck(deckNumber);
+                if (deck.Ydk == "Deck Not Found")
+                {
+                    Console.WriteLine("Deck Not found");
+                }
+                else
+                {
+                    Console.WriteLine(String.Concat(deck.Builder, "-", deck.Type, "-", deckNumber) ?? deckNumber);
+                    Console.WriteLine(deck.Ydk);
+                }
+                deckNumber = Console.ReadLine();
+            }
+        }
+
+        public static async Task<Deck?> CreateDeck(string deckNumber)
+        {
+            //string url = $"https://www.formatlibrary.com/api/decks/download/{deckNumber}";
+            //string pathReflection = System.Reflection.Assembly.GetExecutingAssembly().Location;
+            string? pathReflection = Process.GetCurrentProcess().MainModule?.FileName;
+            string? path = System.IO.Path.GetDirectoryName(pathReflection);
+            Deck? deck = await GetDeck(deckNumber);
+
+            if (path != null && deck != null && !String.IsNullOrEmpty(deck.Ydk) && deck.Ydk != "Deck Not Found")
+            {
+                await using StreamWriter sw = File.CreateText(Path.Combine(path, String.Concat(deck.Builder,"-", deck.Type,"-", deckNumber,".ydk") ?? deckNumber));
+                await sw.WriteAsync(deck.Ydk);
+                await sw.DisposeAsync();
+            }
+
+            return deck;
+        }
+
+        private static async Task<Deck?> GetDeck(string deckNumber)
+        {
+            Deck? deck = new Deck();
+            HttpResponseMessage? response;
+            using HttpClient client = new HttpClient();
+
+            string url = $"https://www.formatlibrary.com/api/decks/{deckNumber}";
+
+            response = await client.GetAsync(url);
+
+            if (String.IsNullOrEmpty(await response.Content.ReadAsStringAsync()) || response.StatusCode == HttpStatusCode.NotFound)
+            {
+                return new Deck(){Ydk = "Deck Not Found"};
+            }
+
+            string deckJson = await response.Content.ReadAsStringAsync();
+
+            deck = JsonSerializer.Deserialize<Deck>(deckJson, new JsonSerializerOptions()
+            {
+                PropertyNameCaseInsensitive = true,
+            });
+
+            deck.Ydk = deck.Ydk.Replace($"created", "");
+            deck.Ydk = deck.Ydk.Replace("by...", "");
+            deck.Ydk = deck.Ydk.Trim();
+
+            return deck;
+        }
+    }
+}
